adb7e7e880fee64732ba1afec3352ce21cff9545
Jonathan Eyolfson <jon@eyolfson.com>
2022-09-14 16:07:53 -0400

    Included multiple fork example

--- lecture-04/fork-example.c
+++ lecture-04/fork-example.c
@@ -5,21 +5,21 @@
 
 int main(int argc, char *argv[]) {
   pid_t pid = fork();
-  if (pid == -1) {
-    int err = errno;
-    perror("fork failed");
-    return err;
+  if (pid > 0) {
+    printf("Parent returned pid: %d\n", pid);
+    printf("Parent pid: %d\n", getpid());
+    printf("Parent parent pid: %d\n", getppid());
+    usleep(1000);
   }
-  if (pid == 0) {
+  else if (pid == 0) {
     printf("Child returned pid: %d\n", pid);
     printf("Child pid: %d\n", getpid());
     printf("Child parent pid: %d\n", getppid());
   }
   else {
-    printf("Parent returned pid: %d\n", pid);
-    printf("Parent pid: %d\n", getpid());
-    printf("Parent parent pid: %d\n", getppid());
-    usleep(1000);
+    int err = errno;
+    perror("fork failed");
+    return err;
   }
   return 0;
 }
