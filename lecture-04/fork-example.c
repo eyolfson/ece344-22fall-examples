5e9f12296d14f2993fdf061e86b90c60888fec79
Jonathan Eyolfson <jon@eyolfson.com>
2022-09-14 11:45:55 -0400

    Added fork example

---
+++ lecture-04/fork-example.c
@@ -0,0 +1,25 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  pid_t pid = fork();
+  if (pid == -1) {
+    int err = errno;
+    perror("fork failed");
+    return err;
+  }
+  if (pid == 0) {
+    printf("Child returned pid: %d\n", pid);
+    printf("Child pid: %d\n", getpid());
+    printf("Child parent pid: %d\n", getppid());
+  }
+  else {
+    printf("Parent returned pid: %d\n", pid);
+    printf("Parent pid: %d\n", getpid());
+    printf("Parent parent pid: %d\n", getppid());
+    usleep(100);
+  }
+  return 0;
+}
