a5af4a5db148a584db54d9bf8e1c646f02491721
Jonathan Eyolfson <jon@eyolfson.com>
2022-09-15 11:05:08 -0400

    Added Lecture 5 examples

---
+++ lecture-05/orphan-example.c
@@ -0,0 +1,23 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  pid_t pid = fork();
+  if (pid == -1) {
+    int err = errno;
+    perror("fork failed");
+    return err;
+  }
+  if (pid == 0) {
+    printf("Child parent pid: %d\n", getppid());
+    sleep(2);
+    printf("Child parent pid (after sleep): %d\n", getppid());
+  }
+  else {
+    sleep(1);
+  }
+  return 0;
+}
